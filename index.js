c83ac67e455deaffee49b1e817ec5e13311aa09d
juhairulislam <juhairulislam2018@gmail.com>
2026-05-06 08:17:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const port =process.env.PORT || 8000 ;
+
+app.use(cors()) ;
+app.use(express.json()) ;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from home World!')
+})
+
+const users = [
+    {
+    id:1 ,
+    name: 'joan doe' ,
+    age: 25,
+    email: 'joan@gmail.com'
+} ,
+
+{ id:2 ,
+    name: 'hoan doe' ,
+    age: 27,
+    email: 'hoan@gmail.com'
+
+} ,
+{
+     id:3 ,
+    name: 'toan doe' ,
+    age: 28,
+    email: 'toan@gmail.com'
+}
+
+]
+
+app.get('/users', (req, res) => {
+  res.send(users)
+})
+
+app.post('/users' , (req, res) =>{
+
+    console.log('data in the request' , req.body) ;
+
+    const newUser = req.body ;
+    newUser.id = newUser.length + 1 ;
+    users.push(newUser)
+
+    res.send({success: true , data: newUser , message: 'User added successfully'}) 
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
